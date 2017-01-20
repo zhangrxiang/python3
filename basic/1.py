3de91fe5c3e8a2fbc897c15f71f588806f5e8ec4
zhangrxiang <zhangrxiang@yahoo.com>
2017-01-20 20:57:44 +0800

    ubuntu pc

--- basic/1.py
+++ basic/1.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
 a = 10 // 3
 print(a)
 
@@ -17,23 +21,23 @@ a = 10 + 3
 print(a)
 
 print(ord("A"))
-print(ord("中"))
-print(chr(ord("中")))
+# print(ord("中"))
+# print(chr(ord("中")))
 
 print("\u4e2d\u6587")
 
 print("ABC".encode('ascii'))
 print("ABC".encode('utf-8'))
 
-print("中文".encode("utf-8"))
+# print("中文".encode("utf-8"))
 
 print(b'ABC'.decode("ascii"))
 print(b'\xe4\xb8\xad\xe6\x96\x87'.decode("utf-8"))
-print("张荣响".encode("utf-8").decode("utf-8"))
+# print("张荣响".encode("utf-8").decode("utf-8"))
 
 # len()函数计算的是str的字符数，如果换成bytes，len()函数就计算字节数：
 print(len("卧槽")) # 2
-print(len("卧槽".encode("utf-8"))) # 6
+# print(len("卧槽".encode("utf-8"))) # 6
 
 # %d	整数
 print("整数 %d" % 10)
